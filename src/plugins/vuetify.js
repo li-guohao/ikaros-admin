906c10787a3fb7df1319888848e9f804e6b25bcd
liguohao_cn <liguohao_cn@qq.com>
2021-01-10 09:50:16 +0800

    配置主题颜色

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: { 
+        dark: false,//是否开启深色模式
+        themes: {
+            light: { 
+              primary: colors.blueGrey.lighten2,
+              secondary: colors.grey.darken1,
+              accent: colors.shades.black,
+              error: colors.red.accent3,
+            }, // 浅色模式颜色配置
+            dark: {
+              primary: colors.shades.black,
+            },// 深色模式颜色配置
+          },
+     }, 
 });
