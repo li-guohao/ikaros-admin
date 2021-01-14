1b7946b9f675016586e7ced8b49d67d2168b4ad1
liguohao_cn <liguohao_cn@qq.com>
2021-01-14 22:34:20 +0800

    移除ant-design-vue, 垃圾antd 反人类布局设计 浪费老子几个小时时间

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,11 @@
 <template>
-  <!-- <div class="root">
+  <div class="root">
     <router-view/>
-  </div> -->
-  <a-layout class="ikaros-layout">
-    <!-- 左边LOGO和导航栏 -->
-    <a-layout-sider
-      breakpoint="lg"
-      collapsed-width="0"
-      @collapse="onCollapse"
-      @breakpoint="onBreakpoint"
-    >
-      <!-- Ikaros Admin Logo -->
-      <div class="ikaros-sider-logo">
-        <a href="#">
-          <span>Ikaros-Admin</span>
-        </a>
-      </div>
-      <!-- 左侧导航栏 -->
-      <a-menu theme="dark" mode="inline" :default-selected-keys="['4']">
-        <a-menu-item key="1">
-          <a-icon type="user" />
-          <span class="nav-text">nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span class="nav-text">nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span class="nav-text">nav 3</span>
-        </a-menu-item>
-        <a-menu-item key="4">
-          <a-icon type="user" />
-          <span class="nav-text">nav 4</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    
-    <!-- 右边 -->
-    <a-layout>
-      <!-- 头部 -->
-      <a-layout-header class="ikaros-layout-header">
-          这是ikaros头部区域
-      </a-layout-header>
-
-      <a-layout style="padding: 0 24px 24px">
-        <!-- 面包屑导航 -->
-        <a-breadcrumb  class="ikaros-layout-breadcrumb">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <!-- 主体内容 -->
-        <a-layout-content class="ikaros-layout-content">
-          <a-card style="width: 300px;display:inline;">
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-            Content
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-          </a-card>
-          
-        </a-layout-content>
-      </a-layout>
-      <!-- 尾部声明 -->
-      <!-- <a-layout-footer style="textAlign: center">
-        Ikaros CMS Web System ©2021 Created by <a href="https://liguohao.cn" target="_blank">liguohao</a>
-      </a-layout-footer> -->
-    </a-layout>
-  </a-layout>
+    <!-- 尾部声明 -->
+    <!-- 
+      Ikaros CMS Web System ©2021 Created by <a href="https://liguohao.cn" target="_blank">liguohao</a>
+    -->
+  </div>
 </template>
 
 <script>
@@ -173,11 +59,7 @@ color: #268deede;
 
   // 主体内容区域
   .ikaros-layout-content{
-    // background-color: #fff;
     display: inline;
-    // padding: 24px;
-    // margin: 5px;
-    // min-height: 280px;
     height: auto;
   }
 }
