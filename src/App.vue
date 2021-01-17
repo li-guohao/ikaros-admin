b9b0de78ee59ee531af66286bce841896b83eaa3
liguohao_cn <liguohao_cn@qq.com>
2021-01-17 10:22:17 +0800

    修改导航链接

--- src/App.vue
+++ src/App.vue
@@ -8,61 +8,66 @@
         <!-- 下拉导航栏 -->
         <el-menu :default-openeds="['1', '3']">
           <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
+            <template slot="title"><i class="el-icon-document-copy"></i>个人记录</template>
+            <el-submenu index="1-1">
+              <template slot="title">最近动态</template>
+              <el-menu-item index="1-1-1">发布动态</el-menu-item>
+              <el-menu-item index="1-1-2">动态管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="1-2">
+              <template slot="title">知识专栏</template>
+              <el-menu-item index="1-2-1">提炼专栏</el-menu-item>
+              <el-menu-item index="1-2-2">专栏管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="1-3">
+              <template slot="title">音乐收听</template>
+              <el-menu-item index="1-3-1">添加歌曲</el-menu-item>
+              <el-menu-item index="1-3-2">歌曲管理</el-menu-item>
+              <el-menu-item index="1-3-3">构建新歌单</el-menu-item>
+              <el-menu-item index="1-3-4">歌单管理</el-menu-item>
+            </el-submenu>
             <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              <template slot="title">视频生活</template>
+              <el-menu-item index="1-4-1">发布VLOG</el-menu-item>
+              <el-menu-item index="1-4-1">VLOG管理</el-menu-item>
             </el-submenu>
           </el-submenu>
           <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <template slot="title"><i class="el-icon-folder-opened"></i>数据中心</template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
+              <template slot="title">文件管理</template>
+              <el-menu-item index="2-1">上传文件</el-menu-item>
+              <el-menu-item index="2-2">所有文件</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
           </el-submenu>
+
           <el-submenu index="3">
-            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+            <template slot="title"><i class="el-icon-user-solid"></i>我的好友</template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
+              <template slot="title">好友管理</template>
+              <el-menu-item index="3-1">添加好友</el-menu-item>
+              <el-menu-item index="3-2">同意请求</el-menu-item>
+              <el-menu-item index="3-3">所有好友</el-menu-item>
+              <el-menu-item index="3-3">好友权限</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title"><i class="el-icon-setting"></i>系统设置</template>
+            <el-menu-item-group>
+              <template slot="title">个人中心</template>
+              <el-menu-item index="4-1">个人资料</el-menu-item>
+              <el-menu-item index="4-2">账号绑定</el-menu-item>
             </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
+            <el-menu-item-group title="系统配置">
+              <el-menu-item index="4-3">配置管理</el-menu-item>
+              <el-menu-item index="4-4">友情链接</el-menu-item>
+              <el-menu-item index="4-5">首页导航</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="三方登录">
+              <el-menu-item index="4-6">QQ登录</el-menu-item>
+              <el-menu-item index="4-7">微信登录</el-menu-item>
+            </el-menu-item-group>
           </el-submenu>
         </el-menu>
       </el-aside>
